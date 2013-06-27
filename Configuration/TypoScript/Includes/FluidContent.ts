912792c2989d296051a10d2f129a777b106c70e8
Inkdpixels <inkdpixels@googlemail.com>
2013-06-27 15:40:43 +0200

    Add a basic setup for FluidContent and FluidPages

---
+++ Configuration/TypoScript/Includes/FluidContent.ts
@@ -0,0 +1,5 @@
+plugin.tx_fed.fce.faehreTemplate {
+	templateRootPath = EXT:t3b_frontend/Resources/Private/Elements/
+	partialRootPath = EXT:t3b_frontend/Resources/Private/Partials/
+	layoutRootPath = EXT:t3b_frontend/Resources/Private/Layouts/
+}
---
+++ Configuration/TypoScript/Includes/FluidPages.ts
@@ -0,0 +1,5 @@
+plugin.tx_fed.page.faehreTemplate {
+	templateRootPath = EXT:t3b_frontend/Resources/Private/Templates/
+	partialRootPath = EXT:t3b_frontend/Resources/Private/Partials/
+	layoutRootPath = EXT:t3b_frontend/Resources/Private/Layouts/
+}
