817b1bbae7fa37ae5ab18a81575521ebbe92942d
Inkdpixels <inkdpixels@googlemail.com>
2013-07-01 23:02:04 +0200

    [BUGFIX] Fix the  paths leading to the TypoScript files for this extension

--- Configuration/TypoScript/Includes/FluidPages.ts
+++ Configuration/TypoScript/Includes/FluidPages.ts
@@ -1,4 +1,4 @@
-plugin.tx_fed.page.faehreTemplate {
+plugin.tx_fed.page.t3bTemplate {
 	templateRootPath = EXT:t3b_template/Resources/Private/Templates/
 	partialRootPath = EXT:t3b_template/Resources/Private/Partials/
 	layoutRootPath = EXT:t3b_template/Resources/Private/Layouts/
