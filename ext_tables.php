a279c6b7b2ceadf5727299cfc13edd128d92aebf
Inkdpixels <inkdpixels@googlemail.com>
2013-07-02 13:16:53 +0200

    [BUGFIX] Add the missing TypoScript include-setup inside ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -3,3 +3,5 @@
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 't3b_template');
