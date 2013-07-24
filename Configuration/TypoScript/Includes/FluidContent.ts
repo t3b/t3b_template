a3ceba1270702cf67f567894d678d5b53d82023e
Inkdpixels <inkdpixels@googlemail.com>
2013-07-24 12:39:22 +0200

    [BUGFIX] Fix the FlduiContent and FluidPages path setup as the register-process was recently updated

--- Configuration/TypoScript/Includes/FluidContent.ts
+++ Configuration/TypoScript/Includes/FluidContent.ts
@@ -1,4 +1,4 @@
-plugin.tx_fluidcontent.view {
+plugin.tx_fluidcontent.collections.t3b_template {
 	templateRootPath = EXT:t3b_template/Resources/Private/Elements/
 	partialRootPath = EXT:t3b_template/Resources/Private/Partials/
 	layoutRootPath = EXT:t3b_template/Resources/Private/Layouts/
--- Configuration/TypoScript/Includes/FluidPages.ts
+++ Configuration/TypoScript/Includes/FluidPages.ts
@@ -1,4 +1,4 @@
-plugin.tx_fluidpages.view {
+plugin.tx_fluidpages.collections.t3b_template {
 	templateRootPath = EXT:t3b_template/Resources/Private/Templates/
 	partialRootPath = EXT:t3b_template/Resources/Private/Partials/
 	layoutRootPath = EXT:t3b_template/Resources/Private/Layouts/
