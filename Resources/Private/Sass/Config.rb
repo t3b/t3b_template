514ea210e1ec99c8a101f5e4f022d6d69e902248
Tyll Weiß <inkdpixels@googlemail.com>
2013-11-07 17:12:31 +0100

    [TASK] Move all ruby functions into the Config.rb file to avoid test errors on Travis CI

--- Resources/Private/Sass/Config.rb
+++ Resources/Private/Sass/Config.rb
@@ -13,7 +13,15 @@
 # =============================================================================
 # RUBY FUNCTIONS
 # =============================================================================
-require File.join(File.dirname(__FILE__), 'Functions.rb')
+# Timestamp function
+#
+# @author 'Hopper' <http://stackoverflow.com/users/1026353/hopper>
+# @link http://stackoverflow.com/questions/13022461/add-timestamps-to-compiled-sass-scss
+module Sass::Script::Functions
+    def timestamp()
+        return Sass::Script::String.new(Time.now.to_s)
+    end
+end
 
 
 # =============================================================================
