c825cd02470a67198b9267a6389ec47561dc5e8a
Tyll Weiß <inkdpixels@googlemail.com>
2013-11-18 14:01:23 +0100

    [DOCS] Remove an unnecessary comment on top of the Compass variables

--- Resources/Private/Sass/Config.rb
+++ Resources/Private/Sass/Config.rb
@@ -32,7 +32,6 @@ end
 # =============================================================================
 # COMPASS PATHS
 # =============================================================================
-# Set this to the root of your project when deployed:
 http_path       = "/"
 sass_dir        = "/"
 css_dir         = "../../Public/Stylesheets"
