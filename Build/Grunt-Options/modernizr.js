67a6f0e3ab49f0373d9afa0d0a52ef93d7d7ac08
Tyll Weiß <weiss@sitegeist.de>
2014-02-11 17:38:21 +0100

    [BUGFIX] Upgrade to grunr-modernizr 0.5.1 to avoid an 404 error while fetching the latest modernizr build

--- Build/Grunt-Options/modernizr.js
+++ Build/Grunt-Options/modernizr.js
@@ -7,33 +7,37 @@
 var config = require('../Config');
 
 module.exports = {
-	devFile: config.JavaScripts.devDir + '/Libaries/Modernizr/modernizr.js',
-	outputFile: config.JavaScripts.distDir + '/Libaries/Modernizr-Custom.js',
+	main: {
+		devFile: config.JavaScripts.devDir + '/Libaries/Modernizr/modernizr.js',
+		outputFile: config.JavaScripts.distDir + '/Libaries/Modernizr-Custom.js',
 
-	// Based on default settings on http://modernizr.com/download/
-	extra: {
-		shiv: true,
-		printshiv: false,
-		load: true,
-		mq: false,
-		cssclasses: true
-	},
-	// Based on default settings on http://modernizr.com/download/
-	extensibility: {
-		addtest: false,
-		prefixed: false,
-		teststyles: false,
-		testprops: false,
-		testallprops: false,
-		hasevents: false,
-		prefixes: false,
-		domprefixes: false
-	},
-	files: [
-		'**/*.{js,css,scss}',
-		'!node_modules/**/*',
-		'!Gruntfile.js',
-		'!' + config.JavaScripts.devDir + '/Libaries/**/*',
-		'!' + config.JavaScripts.distDir + '/Libaries/**/*'
-	]
+		// Based on default settings on http://modernizr.com/download/
+		extra: {
+			shiv: true,
+			printshiv: false,
+			load: true,
+			mq: false,
+			cssclasses: true
+		},
+		// Based on default settings on http://modernizr.com/download/
+		extensibility: {
+			addtest: false,
+			prefixed: false,
+			teststyles: false,
+			testprops: false,
+			testallprops: false,
+			hasevents: false,
+			prefixes: false,
+			domprefixes: false
+		},
+		files : {
+			"src": [
+				'**/*.{js,css,scss}',
+				'!node_modules/**/*',
+				'!Gruntfile.js',
+				'!' + config.JavaScripts.devDir + '/Libaries/**/*',
+				'!' + config.JavaScripts.distDir + '/Libaries/**/*'
+			]
+		}
+	}
 };
