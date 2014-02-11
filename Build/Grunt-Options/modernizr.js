8e33a4bc9caf2cea98a9bf84bd33cd0353d350d0
Tyll Weiß <inkdpixels@googlemail.com>
2014-02-11 22:28:36 +0100

    [TASK] Create a 'modernizr' options obj in the grunt config file to avoid edits in the options file itself

--- Build/Grunt-Options/modernizr.js
+++ Build/Grunt-Options/modernizr.js
@@ -12,24 +12,12 @@ module.exports = {
 		outputFile: config.JavaScripts.distDir + '/Libaries/Modernizr-Custom.js',
 
 		// Based on default settings on http://modernizr.com/download/
-		extra: {
-			shiv: true,
-			printshiv: false,
-			load: true,
-			mq: false,
-			cssclasses: true
-		},
+		extra: config.Modernizr.extra,
 		// Based on default settings on http://modernizr.com/download/
-		extensibility: {
-			addtest: false,
-			prefixed: false,
-			teststyles: false,
-			testprops: false,
-			testallprops: false,
-			hasevents: false,
-			prefixes: false,
-			domprefixes: false
-		},
+		extensibility: config.Modernizr.extensibility,
+		tests : config.Modernizr.tests,
+		customTests : config.Modernizr.customTests,
+		matchCommunityTests : config.Modernizr.matchCommunityTests,
 		files : {
 			"src": [
 				'**/*.{js,css,scss}',
