79ba14667ff2c27cc380d50da48a4a63fa233143
Tyll Weiß <inkdpixels@googlemail.com>
2014-02-24 20:49:21 +0100

    [MISC] Use Upper camelCase for the 'ConfigCreator' function

--- Build/Config.js
+++ Build/Config.js
@@ -4,7 +4,7 @@
  */
 
 var pkg = require("../package");
-var configCreator = function() {
+var ConfigCreator = function() {
 	"use strict";
 
 	this.package = pkg,
@@ -58,4 +58,4 @@ var configCreator = function() {
 	this.packageIsDefault = this.package.name === "t3b_template"; // Check if the defaults in 'package.json' are customized.
 };
 
-module.exports = new configCreator();
+module.exports = new ConfigCreator();
