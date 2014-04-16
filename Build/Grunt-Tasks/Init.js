4701b035fd45e89b68ccc95de82fe5a94c621fdb
Tyll Weiß <weiss@sitegeist.de>
2014-04-16 17:57:41 +0200

    [BUGFIX] Make sure the 'grunt init' task gets fully tested on Travis CI

--- Build/Grunt-Tasks/Init.js
+++ Build/Grunt-Tasks/Init.js
@@ -15,7 +15,7 @@ module.exports = function(grunt) {
 		// Replace general text-strings and paths.
 		grunt.task.run("replace:preInit");
 
-		if (!helpers.isPackageDefault() || grunt.option('env') !== 'travis') {
+		if (!helpers.isPackageDefault() || grunt.option('env') === 'travis') {
 			/*
 			 * If the package.json contents are edited:
 			 *		=> #1: replace all '<!=  !>' strings and set up the git hooks.
