e9323a2fcacdb03ed67a06886b9c79879924742f
Tyll Weiß <weiss@sitegeist.de>
2014-04-25 17:47:11 +0200

    [FEATURE] Add a living styleguide build system based on node-kss

---
+++ Build/Grunt-Options/styleguide.js
@@ -0,0 +1,26 @@
+/**
+ * Grunt-Contrib-Clean
+ * @description Cleans files and folders.
+ * @docs https://github.com/gruntjs/grunt-contrib-clean
+ */
+
+var config = require("../Config");
+
+module.exports = {
+	options: {
+		template: {
+			src: 'Documentation/Private/Template'
+		},
+		framework: {
+			name: 'kss'
+		}
+	},
+	all: {
+		files: [{
+			'Documentation': config.Sass.cssDir + "/**/*.css"
+		}]
+	}
+};
+
+
+
