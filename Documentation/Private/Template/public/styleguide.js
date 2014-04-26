99799839554a8ed9a48727ffc8ec472e8fe3ea58
Tyll Weiß <inkdpixels@googlemail.com>
2014-04-26 20:35:40 +0200

    [DOCS] Add some minor documentation for the initial styleguide.js file

--- Documentation/Private/Template/public/styleguide.js
+++ Documentation/Private/Template/public/styleguide.js
@@ -3,6 +3,6 @@
 		$kssMenuItems = $kssMenu.find('.kss___nav__item'),
 		$kssMenuActiveItem = $kssMenuItems.eq($kssMenu.data('kss-activemenuindex'));
 
-
+	// Add the active class for the current item in the sideMenu.
 	$kssMenuActiveItem.addClass('kss___nav__item--active')
 })();
