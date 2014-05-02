7e04b0e810ab5caf058a30e2f8f9b7666d17fabd
Tyll Weiß <inkdpixels@googlemail.com>
2014-05-02 09:08:56 +0200

    [DOCS] Fix the file header for the styleguide options file

--- Build/Grunt-Options/styleguide.js
+++ Build/Grunt-Options/styleguide.js
@@ -1,7 +1,7 @@
 /**
- * Grunt-Contrib-Clean
- * @description Cleans files and folders.
- * @docs https://github.com/gruntjs/grunt-contrib-clean
+ * Grunt-Styleguide
+ * @description Universal CSS styleguide generator for grunt. Easily integrate Styledocco or KSS styleguide generation into your development workflow.
+ * @docs https://github.com/indieisaconcept/grunt-styleguide
  */
 
 var config = require("../Config");
