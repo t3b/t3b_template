b4af1bf9e040263d70f436bb33fcfbe5faa8a458
Tyll Weiß <inkdpixels@googlemail.com>
2014-05-17 09:38:18 +0200

    [TASK] Change the order of the docs compile task
    Since the styleguide is now based on top of the generated .css file

--- Build/Grunt-Tasks/Compilers/Docs.js
+++ Build/Grunt-Tasks/Compilers/Docs.js
@@ -7,13 +7,13 @@ module.exports = function(grunt) {
 	"use strict";
 
 	grunt.registerTask("compile:docs", function() {
-		// Create the living CSS styleguide.
-		grunt.task.run("styleguide");
-
 		// Prefix the docs stylesheet.
 		grunt.task.run("autoprefixer:docs");
 
 		// Compile the new stylesheets.
 		grunt.task.run("compile:css:dev");
+
+		// Create the living CSS styleguide.
+		grunt.task.run("styleguide");
 	});
 };
