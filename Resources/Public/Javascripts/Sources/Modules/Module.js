c29643ac30cdcde5998d504edb00bcfdc2ee676c
Tyll Weiß <weiss@sitegeist.de>
2014-06-17 16:38:19 +0200

    [TASK] Convert spaces to tabs for code consistency

--- Resources/Public/Javascripts/Sources/Modules/Module.js
+++ Resources/Public/Javascripts/Sources/Modules/Module.js
@@ -2,12 +2,12 @@ define(function(require) {
 	'use strict';
 
 	var App = function(el) {
-        this.el = el;
-    };
+		this.el = el;
+	};
 
-    App.prototype.render = function() {
-        this.el.innerHTML = 'require.js up and running';
-    };
+	App.prototype.render = function() {
+		this.el.innerHTML = 'require.js up and running';
+	};
 
-    return App;
+	return App;
 });
