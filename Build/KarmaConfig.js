7e914cbabf8cd671a75c3c56555ba2e0846c9917
Tyll Weiß <inkdpixels@googlemail.com>
2014-07-15 20:31:18 +0200

    [DOCS] Add missing punctuation and docs for the karmaConfig

--- Build/KarmaConfig.js
+++ Build/KarmaConfig.js
@@ -9,48 +9,49 @@ module.exports = function (karmaConfig) {
 	'use strict';
 
 	karmaConfig.set({
-		// base path, that will be used to resolve files and exclude
+		// Base path, that will be used to resolve files and exclude.
 		basePath: '../',
 
-		// frameworks to use
+		// Drameworks to use.
     	frameworks: ['jasmine', 'commonjs'],
 
     	// Plugins to load.
 		plugins: ['karma-*'],
 
-		// list of files / patterns to load in the browser
+		// List of files / patterns to load in the browser.
 		files: [
 		  'Resources/Private/Javascripts/Tests/**/*.js',
 		  'Resources/Private/Javascripts/Modules/**/*.js'
 		],
 
-		// test results reporter to use
+		// Test results reporter to use.
 		// possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
 		reporters: ['coverage', 'dots'],
 
-		// enable / disable colors in the output (reporters and logs)
+		// Enable / Disable colors in the output (reporters and logs).
 		colors: true,
 
-		// level of logging
+		// Level of logging.
 		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
 		logLevel: karmaConfig.LOG_ERROR,
 
-		// enable / disable watching file and executing tests whenever any file changes
+		// Enable / Disable watching file and executing tests whenever any file changes.
 		autoWatch: false,
 
-		// If browser does not capture in given timeout [ms], kill it
+		// If browser does not capture in given timeout [ms], kill it.
 		captureTimeout: 60000,
 
 		// Continuous Integration mode
-		// if true, it capture browsers, run tests and exit
+		// if true, it capture browsers, run tests and exits.
 		singleRun: true,
 
-		// Add browserify to preprocessors
+		// Add commonJS support and create the code coverage.
 		preprocessors: {
 			'Resources/Private/Javascripts/Modules/**/*.js': ['commonjs', 'coverage'],
 			'Resources/Private/Javascripts/Tests/**/*.js': ['commonjs'],
 		},
 
+		// Specify the location of the coverage report.
 		coverageReporter: {
 			reporters: [{
 				type: 'text-summary',
